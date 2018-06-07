7cce379682ec4a8b7512c59e36fd9857f891b7a1
晃弘 上山 <akihiro@hololab.co.jp>
2018-06-08 02:42:24 +0900

    first commit パッケージ化

---
+++ PhotoFileControl/Assets/PhotoFileControl/Scenes/PhotoFileControlSample.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using PhotoFileControl;
+
+public class PhotoFileControlSample : MonoBehaviour {
+    public GameObject panel;
+
+	// Use this for initialization
+	void Start () {
+        StartCoroutine(PhotoView());
+	}
+
+    private IEnumerator PhotoView()
+    {
+        // View List
+        string[] list = null;
+        yield return PhotoControl.ViewPhotos((l) => { list = l; });
+        for (int i = 0; i < list.Length; i++) Debug.Log(list[i]);
+
+        // Load Photo
+        Texture2D tex = new Texture2D(1, 1);
+        yield return PhotoControl.LoadPhoto(list[0], (t) => { tex = t; });
+        panel.GetComponent<Renderer>().material.mainTexture = tex;
+
+        // Save Photo
+        yield return PhotoControl.SavePhoto("test", tex, null);
+    }
+}
---
+++ PhotoFileControl/Assets/PhotoFileControl/Scripts/PhotoControl.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading;
+using UnityEngine;
+#if UNITY_UWP
+using System.Threading.Tasks;
+using Windows.Storage;
+using Windows.Graphics.Imaging;
+#elif UNITY_EDITOR || UNITY_STANDALONE
+#endif
+
+namespace PhotoFileControl
+{
+    public class PhotoControl
+    {
+        public static IEnumerator ViewPhotos(Action<string[]> action)
+        {
+            List<string> list = new List<string>();
+#if UNITY_UWP
+            Task task = Task.Run(async() =>
+            {
+                var storagefilelist = await KnownFolders.CameraRoll.GetFilesAsync();
+                for (int i = 0; i < storagefilelist.Count; i++)
+                {
+                    list.Add(storagefilelist[i].Name);
+                }
+            });
+            yield return new WaitWhile(() => task.IsCompleted == false);
+#elif UNITY_EDITOR || UNITY_STANDALONE
+            var path = FileIOControl.LocalFolderPath;
+            Thread thread = new Thread(() => {
+                var dir=Directory.GetFiles(path);
+                for (int i = 0; i < dir.Length; i++)
+                {
+                    list.Add(Path.GetFileName(dir[i]));
+                }
+            });
+            thread.Start();
+            yield return new WaitWhile(() => thread.IsAlive == true);
+#endif
+            yield return null;
+            if (action != null) action.Invoke(list.ToArray());
+        }
+
+        public static IEnumerator SavePhoto(string name, Texture2D tex,Action action)
+        {
+            var data = tex.EncodeToPNG();
+#if UNITY_UWP
+            Task task = Task.Run(async () =>
+            {
+                var file = await KnownFolders.CameraRoll.CreateFileAsync(name + ".png", CreationCollisionOption.ReplaceExisting);
+                await FileIO.WriteBytesAsync(file, data);
+            });
+            yield return new WaitWhile(() => task.IsCompleted == false);
+#elif UNITY_EDITOR || UNITY_STANDALONE
+            yield return FileIOControl.WriteBytesFile(FileIOControl.LocalFolderPath + "\\" + name + ".png", data);
+#endif
+            yield return null;
+            if (action != null) action.Invoke();
+        }
+
+        public static IEnumerator LoadPhoto(string name,Action<Texture2D> action)
+        {
+            Texture2D tex = null;
+            byte[] data = null;
+#if UNITY_UWP
+            uint w = 0, h = 0;
+            Task task = Task.Run(async () =>
+            {
+                var storagefilelist = await KnownFolders.CameraRoll.GetFilesAsync();
+                foreach (var item in storagefilelist)
+                {
+                    if (item.Name == name)
+                    {
+                        var img = await item.OpenReadAsync();
+                        var decoder = await BitmapDecoder.CreateAsync(img);
+                        var pixel = await decoder.GetPixelDataAsync();
+                        var bytes = pixel.DetachPixelData();
+                        w = decoder.PixelWidth;
+                        h = decoder.PixelHeight;
+                        data = new byte[bytes.Length];
+                        for (int i = 0; i < h; i++)
+                        {
+                            for (int j = 0; j < w; j++)
+                            {
+                                data[(w * (h - 1 - i) + j) * 4 + 0] = bytes[(w * i + j) * 4 + 0];
+                                data[(w * (h - 1 - i) + j) * 4 + 1] = bytes[(w * i + j) * 4 + 1];
+                                data[(w * (h - 1 - i) + j) * 4 + 2] = bytes[(w * i + j) * 4 + 2];
+                                data[(w * (h - 1 - i) + j) * 4 + 3] = bytes[(w * i + j) * 4 + 3];
+                            }
+                        }
+                        break;
+                    }
+                }
+            });
+            yield return new WaitWhile(() => task.IsCompleted == false);
+            tex = new Texture2D((int)w, (int)h, TextureFormat.BGRA32, false);
+            tex.LoadRawTextureData(data);
+#elif UNITY_EDITOR || UNITY_STANDALONE
+            tex = new Texture2D(1, 1);
+            yield return FileIOControl.ReadBytesFile(FileIOControl.LocalFolderPath + "\\" + name, (b) => { data = b; });
+            tex.LoadImage(data);
+#endif
+            tex.Apply();
+            yield return null;
+            if (action != null) action.Invoke(tex);
+        }
+    }
+}
